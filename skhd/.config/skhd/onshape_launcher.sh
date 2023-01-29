1886df53094ccc29971e8433552074859fa68839
Philip Büchler <philip.buechler@gmail.com>
2023-01-30 00:45:41 +0100

    Skdh setup update to work with HHKBxPlanck keyboard

--- skhd/.config/skhd/onshape_launcher.sh
+++ skhd/.config/skhd/onshape_launcher.sh
@@ -7,7 +7,7 @@ if [ "$is_open" == '"Firefox"' ]
 then
    yabai -m space --focus 3
 else
-   /Applications/Firefox.app/Contents/MacOS/firefox --new-window https://www.onshape.com/ \
+   /Applications/Firefox.app/Contents/MacOS/firefox --new-window https://cad.onshape.com/ \
             && osascript -e activate application "Firefox" \
             && sleep 1 \
             && yabai -m window --space 3;  yabai -m space --focus 3
